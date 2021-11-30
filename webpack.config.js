2cf5e37723af2c7ec41e2e4dc3930632feef71f3
Woo KyungJun <natcho9010@gmail.com>
2021-11-30 15:48:44 +0900

    [config] dev server auto open

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ module.exports = {
   },
   devServer: {
     port: 3000,
+    open: true,
   },
   plugins: [
     new HtmlWebpackPlugin({ template: path.resolve(__dirname, "src", "index.html") }),
